fe21439a771a010b4de200eb82d6998ce526421a
Lucas <lucasgf@hotmail.com>
2019-02-18 14:20:57 -0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined'
+  });
+
   socket.on('createMessage', (message) => {
     console.log('Created message:', message)
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
